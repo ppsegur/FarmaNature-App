2f09cc3f3bd168ba2eacd9f8b4593dceb8bf8f22
ppsegur <pepesegurarodriguez@gmail.com>
2025-02-18 09:50:04 +0100

    comentarios con ideas

--- farma-app/src/main/java/com/salesianostriana/dam/farma_app/servicio/UsuarioService.java
+++ farma-app/src/main/java/com/salesianostriana/dam/farma_app/servicio/UsuarioService.java
@@ -23,6 +23,12 @@ public class UsuarioService {
                 .build();
         return usuarioRepo.save(user);
     }
+    //Deberíamos hacer dos endPoints para crear un cliente o un farmaceútico
+    //Podemos plantearlo paara que cuando sea un farmaceutico el correo de verificacion sea al admin de todas para que hasta que este no confiirme su cuenta no esta verificada
+    //y para los usuarios como los clientes podemos hacer el verificado del Qr con 2factor -->
+
+
+    
 
 }
 
