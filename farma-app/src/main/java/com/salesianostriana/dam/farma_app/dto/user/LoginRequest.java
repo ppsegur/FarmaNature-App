63e4c22defa8e5d3a8120b7aed7fc067e2cc5480
ppsegur <pepesegurarodriguez@gmail.com>
2025-02-18 10:44:35 +0100

    Ampliación de la v1 de seguridad todavia falat

---
+++ farma-app/src/main/java/com/salesianostriana/dam/farma_app/dto/user/LoginRequest.java
@@ -0,0 +1,4 @@
+package com.salesianostriana.dam.farma_app.dto.user;
+
+public record LoginRequest(String username, String password) {
+}
---
+++ farma-app/src/main/java/com/salesianostriana/dam/farma_app/seguridad/refresh/RefreshTokenService.java
@@ -0,0 +1,62 @@
+package com.salesianostriana.dam.farma_app.seguridad.refresh;
+
+
+import com.salesianostriana.dam.farma_app.dto.user.UserResponse;
+import com.salesianostriana.dam.farma_app.modelo.Usuario;
+import com.salesianostriana.dam.farma_app.repositorio.UsuarioRepo;
+import com.salesianostriana.dam.farma_app.seguridad.access.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final UsuarioRepo userRepository;
+    private final JwtService jwtService;
+
+    @Value("${jwt.refresh.duration}")
+    private int durationInMinutes;
+
+    public RefreshToken create(Usuario user) {
+        refreshTokenRepository.deleteByUser(user);
+        return refreshTokenRepository.save(
+                RefreshToken.builder()
+                        .user(user)
+                        //.user(u)
+                        //.token(UUID.randomUUID().toString())
+                        .expireAt(Instant.now().plusSeconds(durationInMinutes * 60))
+                        .build()
+        );
+    }
+
+    public RefreshToken verify(RefreshToken refreshToken) {
+        if (refreshToken.getExpireAt().compareTo(Instant.now()) < 0) {
+            refreshTokenRepository.delete(refreshToken);
+            throw new RefreshTokenException("Token de refresco caducado. Por favor, vuelva a loguearse");
+        }
+
+        return refreshToken;
+
+    }
+
+    public UserResponse refreshToken(String token) {
+
+        return refreshTokenRepository.findById(UUID.fromString(token))
+                .map(this::verify)
+                .map(RefreshToken::getUser)
+                .map(user -> {
+                    String accessToken = jwtService.generateAccessToken(user);
+                    RefreshToken refreshedToken = this.create(user);
+                    return UserResponse.of(user, accessToken, refreshedToken.getToken());
+                })
+                .orElseThrow(() -> new RefreshTokenException("No se ha podido refrescar el token. Por favor, vuelva a loguearse"));
+
+    }
+
+}
