9ae3e61293d6401fb934965a034dd7e4a5daefd1
ppsegur <pepesegurarodriguez@gmail.com>
2025-02-23 02:08:12 +0100

    Creación del endPoint para crear una categoria

--- farma-app/src/main/java/com/salesianostriana/dam/farma_app/controlador/CategoriaController.java
+++ farma-app/src/main/java/com/salesianostriana/dam/farma_app/controlador/CategoriaController.java
@@ -1,4 +1,66 @@
 package com.salesianostriana.dam.farma_app.controlador;
 
+import com.salesianostriana.dam.farma_app.dto.EditCategoriaDto;
+import com.salesianostriana.dam.farma_app.dto.GetCategoriaDto;
+import com.salesianostriana.dam.farma_app.dto.user.CreateUserRequest;
+import com.salesianostriana.dam.farma_app.modelo.Categoria;
+import com.salesianostriana.dam.farma_app.servicio.CategoriaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "Categoria", description = "El controlador para los distintas categorias  ")
 public class CategoriaController {
+
+    private  final CategoriaService service;
+
+
+    @Operation(summary = "Registra una nueva categoría ")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Categoria creado con éxito",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = EditCategoriaDto.class),
+                                    examples = @ExampleObject(value = """
+                                {
+                                 
+                                    "nombre": "tés",
+                                
+                                }
+                                """)
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Datos inválidos para registrar la categoria",
+                    content = @Content
+            )
+    })
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/categoria")
+    public ResponseEntity<Categoria> addCategoria(@RequestBody @Valid EditCategoriaDto getCategoriaDto){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.saveCategoria(getCategoriaDto));
+
+    }
 }
--- farma-app/src/main/java/com/salesianostriana/dam/farma_app/dto/EditCategoriaDto.java
+++ farma-app/src/main/java/com/salesianostriana/dam/farma_app/dto/EditCategoriaDto.java
@@ -1,4 +1,11 @@
 package com.salesianostriana.dam.farma_app.dto;
 
-public record EditCategoriaDto() {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record EditCategoriaDto(
+        @NotBlank(message = "El nombre de una categoria puede estar vacía")
+        @Size(max = 255, message = "La dirección no puede tener más de 255 caracteres")
+        String nombre
+) {
 }
