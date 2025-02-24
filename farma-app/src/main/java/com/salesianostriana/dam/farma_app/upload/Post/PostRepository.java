726af1cb9a3700cb3261c1c326115666081b88c8
ppsegur <pepesegurarodriguez@gmail.com>
2025-02-24 13:25:12 +0100

    eliminación de código ejemplo para realizar las imágenes

--- farma-app/src/main/java/com/salesianostriana/dam/farma_app/upload/Post/PostRepository.java
+++
@@ -1,6 +0,0 @@
-package com.salesianostriana.dam.farma_app.upload.Post;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
