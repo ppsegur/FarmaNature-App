b528b9a0a63a12e909a6f5268c33ec5651f2c33a
ppsegur <pepesegurarodriguez@gmail.com>
2025-05-20 20:37:22 +0200

    feat: add CategoriaProductCount record and CorsConfig for CORS settings

--- farma-app/src/main/java/com/salesianostriana/dam/farma_app/dto/CategoriaProductCount.java
+++ farma-app/src/main/java/com/salesianostriana/dam/farma_app/dto/CategoriaProductCount.java
@@ -1,4 +1,9 @@
 package com.salesianostriana.dam.farma_app.dto;
 
-public record CategoriaProductCount() {
+public record CategoriaProductCount(
+        String getNombre,
+        Long getNumProductos
+) {
+
+
 }
