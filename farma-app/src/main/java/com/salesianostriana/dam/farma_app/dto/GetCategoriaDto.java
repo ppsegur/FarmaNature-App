773e1ecda84a320f48bde1d102878ab4b041a7df
ppsegur <pepesegurarodriguez@gmail.com>
2025-05-20 20:36:10 +0200

    refactor: arreglos en bugs

--- farma-app/src/main/java/com/salesianostriana/dam/farma_app/dto/GetCategoriaDto.java
+++ farma-app/src/main/java/com/salesianostriana/dam/farma_app/dto/GetCategoriaDto.java
@@ -4,14 +4,25 @@ import com.salesianostriana.dam.farma_app.modelo.Categoria;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
 @Builder
 public record GetCategoriaDto(
         @NotBlank(message = "El titulito de la categoría no puede estar vacío mas alegría que solo contamos con el nombre")
-        String nombre
+        String nombre,
+        UUID id,
+        Set<GetProductoDto> producto
 ) {
     public static GetCategoriaDto of(Categoria c){
+
         return GetCategoriaDto.builder()
+
                 .nombre(c.getNombre())
+                .id(c.getId())
+
+
                 .build();
 
     }
