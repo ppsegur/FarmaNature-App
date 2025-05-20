f07cb6d1e07ef4c809c82a5ee9c254dd23b1f80d
ppsegur <pepesegurarodriguez@gmail.com>
2025-05-20 20:36:56 +0200

    feat: enhance CategoriaRepo and CategoriaService with new query methods and improved edit logic

--- farma-app/src/main/java/com/salesianostriana/dam/farma_app/repositorio/CategoriaRepo.java
+++ farma-app/src/main/java/com/salesianostriana/dam/farma_app/repositorio/CategoriaRepo.java
@@ -1,12 +1,24 @@
 package com.salesianostriana.dam.farma_app.repositorio;
 
+import com.salesianostriana.dam.farma_app.dto.CategoriaProductCount;
 import com.salesianostriana.dam.farma_app.modelo.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CategoriaRepo extends
         JpaRepository<Categoria, UUID> {
      Categoria findByNombre(String nombre);
 
+
+     @Query("SELECT new com.salesianostriana.dam.farma_app.dto.CategoriaProductCount(c.nombre, COUNT(p)) " +
+             "FROM Categoria c LEFT JOIN c.productos p GROUP BY c ORDER BY COUNT(p) DESC")
+     List<CategoriaProductCount> contarProductosPorCategoria();
+
+     @Query("SELECT new com.salesianostriana.dam.farma_app.dto.CategoriaProductCount(c.nombre, COUNT(p)) " +
+             "FROM Categoria c LEFT JOIN c.productos p GROUP BY c ORDER BY COUNT(p) DESC")
+     List<CategoriaProductCount> topCategoriaPorProducto();
 }
